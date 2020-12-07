1e97e43c60aad32737d0dc8c6f46009bb74dcb59
fdassa <fdassa1@gmail.com>
2020-12-07 15:42:07 -0300

    remove imports desnecessarios

--- lib/blocs/BaseBloc.dart
+++ lib/blocs/BaseBloc.dart
@@ -1,6 +1,4 @@
 import 'package:http/http.dart' as http;
-import 'package:workout_exercises/models/CategoryListResponse.dart';
-import 'package:workout_exercises/repository/Repository.dart';
 
 class BaseBloc {
   final http.Client httpClient = http.Client();
