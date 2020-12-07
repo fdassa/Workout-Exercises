72e9a9f6c028baeafab4d7b5491c0d8b76b85bf9
fdassa <fdassa1@gmail.com>
2020-12-07 12:55:17 -0300

    remove comentario

--- lib/WorkoutLocalizations.dart
+++ lib/WorkoutLocalizations.dart
@@ -80,8 +80,6 @@ class WorkoutLocalizationsDelegate extends LocalizationsDelegate<WorkoutLocaliza
 
   @override
   Future<WorkoutLocalizations> load(Locale locale) {
-    // Returning a SynchronousFuture here because an async "load" operation
-    // isn't needed to produce an instance of DemoLocalizations.
     return SynchronousFuture<WorkoutLocalizations>(WorkoutLocalizations(locale));
   }
 
