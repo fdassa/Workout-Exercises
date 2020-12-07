523612d2fabafbcb350e8365229c2619ad8134d6
fdassa <fdassa1@gmail.com>
2020-12-06 22:16:17 -0300

    envia uma notificação para o usuário caso ele abra o app com menos de 25% de bateria

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:workout_exercises/pages/CategoriesPage.dart';
 
-void main() {
+final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  const AndroidInitializationSettings initializationSettingsAndroid =
+      AndroidInitializationSettings('app_icon');
+  final IOSInitializationSettings initializationSettingsIOS =
+      IOSInitializationSettings(
+          onDidReceiveLocalNotification: onDidReceiveLocalNotification);
+  final MacOSInitializationSettings initializationSettingsMacOS =
+      MacOSInitializationSettings();
+  final InitializationSettings initializationSettings = InitializationSettings(
+      android: initializationSettingsAndroid,
+      iOS: initializationSettingsIOS,
+      macOS: initializationSettingsMacOS);
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
   runApp(MyApp());
 }
 
+Future onDidReceiveLocalNotification(
+    int id, String title, String body, String payload) async {
+  // display a dialog with the notification details, tap ok to go to another page
+}
+
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -28,7 +51,9 @@ class MyApp extends StatelessWidget {
           bodyText2: TextStyle(fontSize: 14.0, fontStyle: FontStyle.italic),
         ),
       ),
-      home: CategoriesPage(),
+      home: CategoriesPage(
+          flutterLocalNotificationsPlugin: flutterLocalNotificationsPlugin
+      ),
     );
   }
 }
--- lib/pages/CategoriesPage.dart
+++ lib/pages/CategoriesPage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:workout_exercises/blocs/CategoriesBloc.dart';
 import 'package:workout_exercises/models/Category.dart';
 import 'package:workout_exercises/models/CategoryListResponse.dart';
@@ -7,12 +9,23 @@ import 'package:workout_exercises/pages/BasePageState.dart';
 import 'package:workout_exercises/pages/ExercisesByCategoryPage.dart';
 
 class CategoriesPage extends StatefulWidget {
+  CategoriesPage({@required this.flutterLocalNotificationsPlugin});
+
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+
   @override
   _CategoriesPageState createState() => _CategoriesPageState();
 }
 
 class _CategoriesPageState extends BasePageState<CategoriesPage> {
   CategoriesBloc _bloc = CategoriesBloc();
+  final platform = const MethodChannel('samples.flutter.dev/battery');
+
+  @override
+  void initState() {
+    _getBatteryLevel();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -66,5 +79,27 @@ class _CategoriesPageState extends BasePageState<CategoriesPage> {
       MaterialPageRoute(
           builder: (context) => ExercisesByCategoryPage(category: category)),
     );
+  }
+
+  Future<void> _showNotification() async {
+    const AndroidNotificationDetails androidPlatformChannelSpecifics =
+        AndroidNotificationDetails(
+            'your channel id', 'your channel name', 'your channel description',
+            importance: Importance.max,
+            priority: Priority.high,
+            ticker: 'ticker');
+    const NotificationDetails platformChannelSpecifics =
+        NotificationDetails(android: androidPlatformChannelSpecifics);
+    await widget.flutterLocalNotificationsPlugin.show(0, 'Low battery',
+        'You have less than 25% battery left!', platformChannelSpecifics);
+  }
+
+  Future<void> _getBatteryLevel() async {
+    try {
+      final int result = await platform.invokeMethod('getBatteryLevel');
+      if (result < 25) {
+        _showNotification();
+      }
+    } on PlatformException catch (e) {}
   }
 }
